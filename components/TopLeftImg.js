17f4750f84d0fa3cd819def73ecd95d42f33bf9c
Akshay Kudalkar <akshaykudalkar.12@gmail.com>
2023-11-22 20:45:59 +0530

    solving build in topleftimg.js

--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -11,9 +11,16 @@ const TopLeftImg = () => {
     };
   }, []);
   
-  return <div className="absolute top-0 left-0 mix-blend-color-dodge z-10 w-[200px] xl:w-[400px] opacity-50">
-    <Image src='/top-left-img.png' width={400} height={400} />
-  </div>;
+  return (
+    <div className="absolute top-0 left-0 mix-blend-color-dodge z-10 w-[200px] xl:w-[400px] opacity-50">
+      <Image
+        src='/top-left-img.png'
+        alt=""
+        width={400}
+        height={400}
+      />
+    </div>
+  );
 };
 
 export default TopLeftImg;
